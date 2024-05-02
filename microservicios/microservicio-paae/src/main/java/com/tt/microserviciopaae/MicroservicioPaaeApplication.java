27ea5f431e53e853a0faf338d41eb81e6127870c
brandongranados <estrem_@hotmail.com>
2024-05-02 11:53:22 -0600

    nuevos microservicios paae y auditor

---
+++ microservicios/microservicio-paae/src/main/java/com/tt/microserviciopaae/MicroservicioPaaeApplication.java
@@ -0,0 +1,13 @@
+package com.tt.microserviciopaae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPaaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioPaaeApplication.class, args);
+	}
+
+}
